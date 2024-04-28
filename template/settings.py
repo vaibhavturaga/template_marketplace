b60cb1f10a2952fb20ef2412dc90cbbca72127a5
Vaibhav Turaga <vaibhavturaga@gmail.com>
2024-04-28 14:18:26 -0400

    deployment changes

--- template/settings.py
+++ template/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9!0#zno48r2o6)&sa43t052jc*07+wlx5pazn4$hn8=+pdoari'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
